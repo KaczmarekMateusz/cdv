091bbc91148e86aa6672b0a72f066ee2bde6cc45
KaczmarekMateusz <mateusz.skrzynka@gmail.com>
2019-04-18 19:58:40 +0200

    a

---
+++ math/math.js
@@ -0,0 +1,19 @@
+const pi = Math.PI;
+let zaokraglij = Math.round(5.6);
+let potega = Math.pow(2,6);
+let pierwiastek = Math.sqrt(2,6);
+let bezwzgledna = Math.abs(-4.5);
+let calkowita = Math.ceil(4.1);//5
+let calkowita1 = Math.floor(4.1);//4
+let minimalna = Math.min(1,-3,10,15);
+let maksymalna = Math.max(1,-3,10,15);
+let losuj =Math.floor(Math.random() * (10 - 2 + 1)+ 2);
+
+// napisz aplikacje, ktora wylosuje liczbe z przedziału podanego przez uzytkownika
+// z klawiatury
+
+
+//wylosuj tyle liczb z przedzialu ile poda uzytkownik z klawiatury, wyswietl je w formacie minimalna, maksymalna, liczba 1:..,liczba2:..      sprawdz czy dane podane przez uzytkownika sa liczbami  
+
+
+console.log(losuj);
